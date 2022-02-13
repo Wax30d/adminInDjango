7e297ca3fab8ede5954d9c2dffa9e12bb5247346
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-13 07:38:37 +0500

    Created Product model.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    mfg_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def show_desc(self):
+        return self.description[:50]
